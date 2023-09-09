c1b01468662172f99a19222f8544dca1c51441f0
Moses Mwaura <moses@Mosess-MacBook-Pro.local>
2023-09-09 15:44:16 -0400

    For uploaded media

--- admin/urls.py
+++ admin/urls.py
@@ -24,4 +24,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('django_nextjs.urls')),
     path('', home, name='home'),
-] + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
