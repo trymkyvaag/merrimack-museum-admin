5465c443b20503bc841e04efd5f837a5180a2ed3
Mark Case <casem@merrimack.edu>
2023-09-14 21:18:12 -0400

    Local DB setup

--- admin/urls.py
+++ admin/urls.py
@@ -20,8 +20,12 @@ from django.contrib import admin
 from django.urls import include, path
 from .views import home
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('django_nextjs.urls')),
-    path('', home, name='home'),
-] + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns = (
+    [
+        path("admin/", admin.site.urls),
+        path("", include("django_nextjs.urls")),
+        path("", home, name="home"),
+    ]
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+)
