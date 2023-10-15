dd74e968e527fc6f70aa6d3b9e0c6d2276e063dd
Mark Case <casem@merrimack.edu>
2023-10-14 21:54:32 -0400

    Add view, exempting image

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from api.models import Artwork, Artist
+from api.models import Artwork, Artist, Donor, Location, Category
 
 
 class ArtistSerializer(serializers.ModelSerializer):
@@ -9,8 +9,29 @@ class ArtistSerializer(serializers.ModelSerializer):
         fields = ["artist_name"]
 
 
+class DonorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Donor
+        fields = ["donor_name"]
+
+
+class LocationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Location
+        fields = ["location"]
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ["category"]
+
+
 class ArtworkSerializer(serializers.ModelSerializer):
     artist_name = serializers.CharField(write_only=True)  # Nested serializer for artist
+    donor_name = serializers.CharField(write_only=True)
+    location = serializers.CharField(write_only=True)
+    category = serializers.CharField(write_only=True)
 
     class Meta:
         model = Artwork
@@ -22,12 +43,32 @@ class ArtworkSerializer(serializers.ModelSerializer):
             "width",
             "height",
             "artist_name",
+            "donor_name",
+            "location",
+            "category",
         )
 
     def create(self, validated_data):
         artist_name = validated_data.pop("artist_name")
+        donor_name = validated_data.pop("donor_name")
+        location_name = validated_data.pop("location")
+        category_name = validated_data.pop("category")
+
         artist_instance, created = Artist.objects.get_or_create(artist_name=artist_name)
-        artwork_instance = Artwork.objects.create(
-            artist=artist_instance, **validated_data
+        donor_instance, created = Donor.objects.get_or_create(donor_name=donor_name)
+        location_instance, created = Location.objects.get_or_create(
+            location=location_name
         )
+        category_instance, created = Category.objects.get_or_create(
+            category=category_name
+        )
+
+        artwork_instance = Artwork.objects.create(
+            artist=artist_instance,
+            donor=donor_instance,
+            location=location_instance,
+            category=category_instance,
+            **validated_data
+        )
+
         return artwork_instance
