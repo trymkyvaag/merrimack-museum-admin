baac397a94cdcbfee3fca219d8e49d1b7050a3ea
Trym Kyvåg <trymkyvag@Tryms-MacBook-Pro.local>
2023-10-17 13:58:27 -0400

    oops

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+class mockTest:
+    def test_addition():
+        assert 1 == 1
+
+    def test_somethingelse():
+        assert True, "this cant break"
+
+
+print("THIS IS A MOCK TEST FOR PIPELINE PURPOSES")
