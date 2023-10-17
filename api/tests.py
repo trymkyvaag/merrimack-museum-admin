652522061051302553feaa27ca2522a5829983eb
Trym Kyvåg <trymkyvag@Tryms-MacBook-Pro.local>
2023-10-17 14:00:04 -0400

    TestCase

--- api/tests.py
+++ api/tests.py
@@ -3,12 +3,12 @@ from django.test import TestCase
 # Create your tests here.
 
 
-class mockTest:
-    def test_addition():
-        assert 1 == 1
+class MyMockTest(TestCase):
+    def test_addition(self):
+        self.assertEqual(1, 1)
 
-    def test_somethingelse():
-        assert True, "this cant break"
+    def test_something_else(self):
+        self.assertTrue(True, "this can't break")
 
 
 print("THIS IS A MOCK TEST FOR PIPELINE PURPOSES")
