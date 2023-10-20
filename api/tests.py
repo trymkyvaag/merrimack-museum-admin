078ddff2794e3a39d7b41948f1fab2af3d41d42f
Trym Kyvåg <trymkyvag@Tryms-MacBook-Pro.local>
2023-10-20 10:06:28 -0400

    test mocktest

--- api/tests.py
+++ api/tests.py
@@ -1,14 +1,14 @@
-from django.test import TestCase
-
-# Create your tests here.
+from unittest import TestCase
 
 
-class MyMockTest(TestCase):
+class TryTesting(TestCase):
+    def test_always_passes(self):
+        self.assertTrue(True)
+
     def test_addition(self):
-        self.assertEqual(1, 1)
-
-    def test_something_else(self):
-        self.assertTrue(True, "this can't break")
+        a = 1
+        b = 1
+        self.assertEqual(a, b)
 
 
 print("THIS IS A MOCK TEST FOR PIPELINE PURPOSES")
