b496363d9dd448e1bc00c792c672d07246d1cb76
Trym Kyvåg <trymkyvag@Tryms-MacBook-Pro.local>
2023-11-06 20:07:21 -0500

    temp

--- Tests/test_add.py
+++ Tests/test_add.py
@@ -3,39 +3,35 @@ from rest_framework import status
 from rest_framework.test import APIClient
 
 from api.models import Artwork
+from api.serializers import ArtworkSerializer
 
 
 class CreateArtworkViewTest(TestCase):
-    def setUp(self):
-        self.client = APIClient()
-
     def test_create_artwork(self):
-        # Create a dictionary representing the data you want to send in the POST request
-        artwork_data = {
-            "title": "Sample Artwork",
+        data = {
+            "title": "My Artwork",
             "date_created_month": 10,
-            "date_created_year": 2023,
-            "comments": "A beautiful artwork",
-            "width": 100,
-            "height": 150,
-            "artist_name": "Sample Artist",
-            "donor_name": "Sample Donor",
-            "location": "Sample Location",
-            "category": "Sample Category",
+            "date_created_year": "2023",
+            "comments": "This is a test artwork",
+            "width": 10.5,
+            "height": 15.2,
+            "artist_name": "John Doe",  # Example artist name
+            "donor_name": "Jane Smith",  # Example donor name
+            "location": "Art Gallery",  # Example location
+            "category": "Painting",  # Example category
         }
 
-        # Send a POST request to your view
-        response = self.client.post(
-            'http://127.0.0.1:8000/api/addartwork/', artwork_data, format='json')
+        serializer = ArtworkSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+        else:
+            self.fail("Serializer is not valid")
 
-        # Check that the response status code is 201 (created)
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        # Add assertions to check if the object is created as expected
+        artwork = Artwork.objects.get(title="My Artwork")
+        self.assertEqual(artwork.title, "My Artwork")
+        self.assertEqual(artwork.date_created_month, 10)
+        # Add more assertions for other fields
 
-        # Check that the data was not saved in the database
-        # Ensure the count is still 0
-        self.assertEqual(Artwork.objects.count(), 0)
-
-    def tearDown(self):
-        # Clean up any data that may have been created during the test
-        # This is not strictly necessary for the transactional test case, but it's good practice
-        Artwork.objects.all().delete()
+        # Clean up after the test
+        artwork.delete()
